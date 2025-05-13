55d349f80a918ffd6f873435cb2b716ccfbf3f21
Jessica <jessica.lopes93@hotmail.com>
2025-05-13 17:42:41 -0300

    adicionando TestantoOfertaValida, TestandoOfertaComRotaNula e TestandoPrecoNegativo

--- JornadaMilhas.Test/UnitTest1.cs
+++ JornadaMilhas.Test/UnitTest1.cs
@@ -1,11 +1,46 @@
+using JornadaMilhasV1.Modelos;
+
 namespace JornadaMilhas.Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestandoOfertaValida()
         {
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.0;
+            var validacao = true;
 
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Equal(validacao, oferta.EhValido);
         }
+
+        [Fact]
+        public void TestandoOfertaComRotaNula()
+        {
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
+            double preco = 100.0;
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+            Assert.False(oferta.EhValido);
+        }
+
+        [Fact]
+        public void TestandoPrecoNegativo()
+        {
+            Rota rota = new Rota("Origem1", "Destino1");
+            Periodo periodo = new Periodo(new DateTime(2024, 8, 20), new DateTime(2024, 8, 30));
+            double preco = -250.0; 
+
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+        }
+
     }
 }
