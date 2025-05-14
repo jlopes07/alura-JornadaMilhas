361eeb3786b7a316503a752bac7bba7840891520
Jessica <jessica.lopes93@hotmail.com>
2025-05-14 14:27:56 -0300

    novos testes e finalização

--- JornadaMilhas.Test/OfertaViagemConstrutor.cs
+++ JornadaMilhas.Test/OfertaViagemConstrutor.cs
@@ -4,13 +4,16 @@ namespace JornadaMilhas.Test
 {
     public class OfertaViagemConstrutor
     {
-        [Fact]
-        public void RetornaOfertaValidaQuandoDadosValidos()
+        [Theory]
+        [InlineData("", null, "2024-01-01", "2024-01-02", 0, false)]
+        [InlineData("OrigemTeste", "DestinoTeste", "2024-02-01", "2024-02-05", 100, true)]
+        [InlineData(null, "São Paulo", "2024-01-01", "2024-01-02", -1, false)]
+        [InlineData("Vitória", "São Paulo", "2024-01-01", "2024-01-01", 0, false)]
+        [InlineData("Rio de Janeiro", "São Paulo", "2024-01-01", "2024-01-02", -500, false)]
+        public void RetornaEhValidoDeAcordoComDadosDeEntrada(string origem, string destino, string dataIda, string dataVolta, double preco, bool validacao)
         {
-            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
-            Periodo periodo = new Periodo(new DateTime(2024, 2, 1), new DateTime(2024, 2, 5));
-            double preco = 100.0;
-            var validacao = true;
+            Rota rota = new Rota(origem, destino);
+            Periodo periodo = new Periodo(DateTime.Parse(dataIda), DateTime.Parse(dataVolta));
 
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
@@ -35,11 +38,27 @@ namespace JornadaMilhas.Test
         {
             Rota rota = new Rota("Origem1", "Destino1");
             Periodo periodo = new Periodo(new DateTime(2024, 8, 20), new DateTime(2024, 8, 30));
-            double preco = -250.0; 
+            double preco = -250.0;
 
             OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
             Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+        }
+
+        [Fact]
+        public void RetornaTresErrosDeValidacaoQuandoRotaPeriodoEPrecoSaoInvalidos()
+        {
+            //arrange
+            int quantidadeEsperada = 3;
+            Rota rota = null;
+            Periodo periodo = new Periodo(new DateTime(2024, 6, 1), new DateTime(2024, 5, 10));
+            double preco = -100;
+
+            //act
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+            //assert
+            Assert.Equal(quantidadeEsperada, oferta.Erros.Count());
         }
 
     }
